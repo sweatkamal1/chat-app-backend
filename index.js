05f61129eff8903361a78b686c7a756f2cb55e03
sweat kamal <sweatkamal152@gmail.com>
2024-09-12 20:49:19 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// const express = require('express')// method-1
-import express from "express"; // method-2
+import express from "express"; 
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
@@ -12,18 +11,16 @@ dotenv.config({});
  
 const PORT = process.env.PORT || 5000;
 
-// middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://chat-app-smoky-two.vercel.app',
+    origin:'http://localhost:3000',
     credentials:true
 };
 app.use(cors(corsOption)); 
 
 
-// routes
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
  
