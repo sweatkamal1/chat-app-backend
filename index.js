4cf038acdf762712357d1a39dc5f5dace8dba100
sweat kamal <sweatkamal152@gmail.com>
2024-09-12 21:06:10 +0530

    commit

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
-};
-app.use(cors(corsOption)); 
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*", // Add your frontend URL or use '*' for all
+    credentials: true
+  }));
+// app.use(cors(corsOption)); 
 
 
 app.use("/api/v1/user",userRoute); 
