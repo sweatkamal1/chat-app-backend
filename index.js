d2fde0a28e9543892c0dc9990a6dfcac12722536
sweat kamal <sweatkamal152@gmail.com>
2024-09-12 21:14:26 +0530

    t6

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import { app,server } from "./socket/socket.js";
 dotenv.config({});
 
  
-const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
@@ -25,6 +24,7 @@ app.use(cors({
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
  
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, ()=>{
     connectDB();
