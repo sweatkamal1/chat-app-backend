de70c4584f92673736671f45fcff3b64368fa63d
sweat kamal <sweatkamal152@gmail.com>
2024-09-13 11:50:28 +0530

    r

--- socket/socket.js
+++ socket/socket.js
@@ -1,37 +1,43 @@
-import {Server} from "socket.io";
-import http from "http";
-import express from "express";
+import { Server } from 'socket.io';
+import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin:['https://chat-app-frontend-hxdu8zm7u-sweat-kamals-projects.vercel.app'],
-        methods:['GET', 'POST'],
+    cors: {
+        origin: [process.env.CLIENT_URL],
+        methods: ['GET', 'POST'],
     },
 });
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
-}
+};
 
-const userSocketMap = {}; // {userId->socketId}
+const userSocketMap = {}; // { userId -> socketId }
 
-
-io.on('connection', (socket)=>{
-    const userId = socket.handshake.query.userId
-    if(userId !== undefined){
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+    if (userId) {
         userSocketMap[userId] = socket.id;
-    } 
+    }
 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
 
-    socket.on('disconnect', ()=>{
+    socket.on('disconnect', () => {
         delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    })
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+});
 
-})
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-export {app, io, server};
+export { app, io, server };
