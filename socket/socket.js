f975aaea8d2172c010fc5760f0c45d57048d6af8
sweat kamal <sweatkamal152@gmail.com>
2024-09-13 13:11:55 +0530

    cf

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000', 'https://chat-app-smoky-two.vercel.app'],
+        origin:['https://chat-app-frontend-rho-swart.vercel.app','http://localhost:3000'],
         methods:['GET', 'POST'],
     },
 });
