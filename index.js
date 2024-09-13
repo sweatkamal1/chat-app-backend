67aaaf8570902a15285b19833c44a66db934b430
sweat kamal <sweatkamal152@gmail.com>
2024-09-13 11:41:18 +0530

    c

--- index.js
+++ index.js
@@ -9,30 +9,25 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-// Connect to the database
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Configure CORS
 app.use(cors({
-    origin: process.env.CLIENT_URL || "*", // Replace '*' with your frontend URL for production
+    origin: process.env.CLIENT_URL || "*",
     credentials: true
 }));
 
-// Define routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
 
-// Global error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000', 'https://chat-app-smoky-two.vercel.app'],
+        origin:['https://chat-app-frontend-hxdu8zm7u-sweat-kamals-projects.vercel.app'],
         methods:['GET', 'POST'],
     },
 });
