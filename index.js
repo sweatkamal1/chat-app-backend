c69c73a48fc6fb3e48afb09a643da86e5f59ffc0
sweat kamal <sweatkamal152@gmail.com>
2024-09-26 11:13:57 +0530

    d

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://chat-app-frontend-rkij.vercel.app',
+    origin:'https://chat-app-frontend-s2hr.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
