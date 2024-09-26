4a2eaf26e3bb9fed4e8e227fa44bf8dfef9b1dfc
sweat kamal <sweatkamal152@gmail.com>
2024-09-26 17:04:17 +0530

    s

--- config/database.js
+++ config/database.js
@@ -2,14 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI); // Remove deprecated options
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1); // Exit with failure
+    process.exit(1); // Stop the server if connection fails
   }
 };
 
